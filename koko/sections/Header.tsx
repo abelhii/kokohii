2f565a4da965e50ec8ea8bb83dd1a55e3b90d3b2
Abel Hii <9299622+abelhii@users.noreply.github.com>
2022-12-05 18:18:58 +0000

    Added slideup title animation

--- koko/sections/Header.tsx
+++ koko/sections/Header.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { APIData, ImageType } from "@shared/types";
 import { getImageUrl } from "@shared/utils";
@@ -21,6 +21,34 @@ const getHeader = async (): Promise<APIData<HeaderType>> => {
   return data;
 };
 
+const LoopingTitleText = ({ titles }: { titles: ReactNode[] }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  useEffect(() => {
+    const loopInterval = setInterval(() => {
+      let currIndex = activeIndex + 1;
+      if (currIndex === titles.length) currIndex = 0;
+      setActiveIndex(currIndex);
+    }, 2000);
+
+    return () => clearInterval(loopInterval);
+  }, [activeIndex]);
+
+  return (
+    <div className="relative inline-block overflow-hidden">
+      {titles.map((title, index) => (
+        <h1
+          key={index}
+          className={`${
+            index !== activeIndex ? "hidden" : ""
+          } xl:text-9xl lg:text-7xl text-5xl max-w-5xl animate-slide-up`}
+        >
+          {title}
+        </h1>
+      ))}
+    </div>
+  );
+};
+
 const HeaderData = ({ name, introduction, subIntro }: HeaderType) => {
   return (
     <>
@@ -33,8 +61,7 @@ const HeaderData = ({ name, introduction, subIntro }: HeaderType) => {
         />
       </div>
       <div className="flex flex-col items-center justify-center h-full w-full gap-40 whitespace-pre-wrap">
-        <h1 className="text-5xl max-w-5xl">{introduction}</h1>
-        <h2 className="text-3xl max-w-lg ml-[50%]">{subIntro}</h2>
+        <LoopingTitleText titles={["Creative Designer", "Based in Ireland"]} />
       </div>
     </>
   );
--- koko/tailwind.config.js
+++ koko/tailwind.config.js
@@ -9,10 +9,20 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'koko-dark': "#3B413C",
-        'koko-light': "#C4D3D1",
-        'project-dark': "#2F2F2F",
-        'project-light': "#E5E5E5",
+        "koko-dark": "#3B413C",
+        "koko-light": "#C4D3D1",
+        "project-dark": "#2F2F2F",
+        "project-light": "#E5E5E5",
+      },
+      keyframes: {
+        "slide-up": {
+          "0%": { transform: "translate3D(0px, 100%, 0px)" },
+          "50%": { transform: "translate3D(0px, 0%, 0px)" },
+          "100%": { transform: "translate3D(0px, -100%, 0px)" },
+        },
+      },
+      animation: {
+        "slide-up": "slide-up 2s ease-in-out",
       },
     },
     fontFamily: {
